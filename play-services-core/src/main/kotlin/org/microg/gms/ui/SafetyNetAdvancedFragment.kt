b838eda4ea199c86778a0c483b062b24c4b071c5
iTrooz <itrooz@protonmail.com>
2022-07-31 22:03:19 +0200

    Migrated SafetyNetAdvancedFragment to Kotlin completely

--- play-services-core/src/main/kotlin/org/microg/gms/ui/SafetyNetAdvancedFragment.kt
+++ play-services-core/src/main/kotlin/org/microg/gms/ui/SafetyNetAdvancedFragment.kt
@@ -5,8 +5,61 @@
 
 package org.microg.gms.ui
 
-import androidx.fragment.app.Fragment
-import com.google.android.gms.R
+//import android.os.Bundle
+//import androidx.fragment.app.Fragment
+//import androidx.preference.Preference
+//import com.google.android.gms.R
+//
+//import org.microg.gms.safetynet.SafetyNetPrefs.PREF_SNET_OFFICIAL
+//import org.microg.gms.safetynet.SafetyNetPrefs.PREF_SNET_SELF_SIGNED
+//import org.microg.gms.safetynet.SafetyNetPrefs.PREF_SNET_THIRD_PARTY
+//import org.microg.tools.ui.AbstractSettingsActivity
+//import org.microg.tools.ui.RadioButtonPreference
+import org.microg.tools.ui.ResourceSettingsFragment
 
-class SafetyNetAdvancedFragment : Fragment(R.layout.safety_net_advanced_fragment) {
+
+class SafetyNetAdvancedFragment : ResourceSettingsFragment() {
+//    private var radioOfficial: RadioButtonPreference? = null
+//    private var radioSelfSigned: RadioButtonPreference? = null
+//    private var radioThirdParty: RadioButtonPreference? = null
+//    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String) {
+//        super.onCreatePreferences(savedInstanceState, rootKey)
+//        radioOfficial = findPreference<Preference>(PREF_SNET_OFFICIAL) as RadioButtonPreference?
+//        radioSelfSigned = findPreference<Preference>(PREF_SNET_SELF_SIGNED) as RadioButtonPreference?
+//        radioThirdParty = findPreference<Preference>(PREF_SNET_THIRD_PARTY) as RadioButtonPreference?
+//    }
+//
+//    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+//        if (preference === radioOfficial) {
+//            radioOfficial!!.isChecked = true
+//            radioSelfSigned!!.isChecked = false
+//            radioThirdParty!!.isChecked = false
+//            return true
+//        } else if (preference === radioSelfSigned) {
+//            radioOfficial!!.isChecked = false
+//            radioSelfSigned!!.isChecked = true
+//            radioThirdParty!!.isChecked = false
+//            return true
+//        } else if (preference === radioThirdParty) {
+//            radioOfficial!!.isChecked = false
+//            radioSelfSigned!!.isChecked = false
+//            radioThirdParty!!.isChecked = true
+//            return true
+//        }
+//        return super.onPreferenceTreeClick(preference)
+//    }
+//
+//    class AsActivity : AbstractSettingsActivity() {
+//        override fun getFragment(): Fragment {
+//            return SafetyNetAdvancedFragment()
+//        }
+//
+//        init {
+//            showHomeAsUp = true
+//        }
+//    }
+//
+//    init {
+//        preferencesResource = R.xml.preferences_snet_advanced
+//    }
 }
